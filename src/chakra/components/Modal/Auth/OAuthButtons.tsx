c5cbb10021eb3531ecf4d431d46c9693e47f3d1d
jojimanos <manosgeorge7@gmail.com>
2023-03-08 09:12:52 +0200

    Save a user in db

--- src/chakra/components/Modal/Auth/OAuthButtons.tsx
+++ src/chakra/components/Modal/Auth/OAuthButtons.tsx
@@ -1,17 +1,33 @@
 import { Flex, Button, Image, Text } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import React from "react";
-import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
-import {auth} from "../../../../firebase/clientApp";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../../../../firebase/clientApp";
 
-export const OAuthButtons:React.FC = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    return (
-        <Flex direction="column" width="100%" mb={4}>
-            <Button variant="oauth" mb={2} isLoading={loading} onClick={() => signInWithGoogle()}>
-            <Image src="/images/google.png" alt="" height={5} width={5}/>
-                Continue with Google</Button>
-            <Button variant="oauth">Some Other Provider</Button>
-            {error && <Text>{error.message}</Text>}
-        </Flex>
-    )
-}
+export const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, userCred, loading, error] =
+    useSignInWithGoogle(auth);
+
+  const createUserDocument = async (user: User) => {
+    const userDocRef = doc(firestore, "users", user.uid);
+
+    await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+  };
+
+  return (
+    <Flex direction="column" width="100%" mb={4}>
+      <Button
+        variant="oauth"
+        mb={2}
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
+        <Image src="/images/google.png" alt="" height={5} width={5} />
+        Continue with Google
+      </Button>
+      <Button variant="oauth">Some Other Provider</Button>
+      {error && <Text>{error.message}</Text>}
+    </Flex>
+  );
+};
--- src/chakra/components/Modal/Auth/SignUp.tsx
+++ src/chakra/components/Modal/Auth/SignUp.tsx
@@ -1,65 +1,116 @@
 import { AuthModalState } from "@/atoms/authModalAtom";
 import { Input, Button, Flex, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
-import {useCreateUserWithEmailAndPassword} from "react-firebase-hooks/auth";
-import {auth} from "../../../../firebase/clientApp"
-import {FIREBASE_ERRORS} from "../../../../firebase/errors";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../../firebase/errors";
+import { addDoc, collection } from "firebase/firestore";
+import { User } from "firebase/auth";
 
-type SignUpProps = {
+type SignUpProps = {};
 
-};
+export const SignUp: React.FC<SignUpProps> = () => {
+  const setAuthModalState = useSetRecoilState(AuthModalState);
 
-export const SignUp:React.FC<SignUpProps> = () => {
-const setAuthModalState = useSetRecoilState(AuthModalState);
+  const [signUpForm, setSignUpForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-const [signUpForm, setSignUpForm] = useState({
-    email: '',
-    password: '',
-    confirmPassword: '',
-})
+  const [formError, setFormError] = useState("");
 
-const [formError, setFormError] = useState('');
+  const [createUserWithEmailAndPassword, userCred, loading, userError] =
+    useCreateUserWithEmailAndPassword(auth);
 
-const [createUserWithEmailAndPassword,
-user,
-loading, 
-userError] = useCreateUserWithEmailAndPassword(auth)
-
-const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(formError) setFormError('');
-    if(signUpForm.password !== signUpForm.confirmPassword)
-    {setFormError("Passwords do not match");
-return;
-}
-    createUserWithEmailAndPassword(signUpForm.email, signUpForm.password)
-    };
+    if (formError) setFormError("");
+    if (signUpForm.password !== signUpForm.confirmPassword) {
+      setFormError("Passwords do not match");
+      return;
+    }
+    createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
+  };
 
-const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSignUpForm((prev) => ({
-        ...prev,
-        [event.target.name]: event.target.value,
+      ...prev,
+      [event.target.name]: event.target.value,
     }));
-};
+  };
 
-    return (
-        <form onSubmit={onSubmit}>
-            <Input required name="email" placeholder="email" type="email" mb={2} onChange={onChange}/>
-            <Input required name="password" placeholder="password" type="password" mb={2} onChange={onChange}/>
-            <Input required name="confirmPassword" placeholder="confirm password" type="confirm password" onChange={onChange}/>
-            {(formError || userError) && <Text textAlign="center" color="red" fontSize="submit">
-            {formError || FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
-            </Text>}
-            <Button width="100%" height="36px" type="submit" mt={2} mb={2} isLoading={loading}>Sign Up</Button>
-            <Flex fontSize="9pt" justifyContent="center">
-                <Text mr={1}>Already a user?</Text>
-                <Text color="blue.500" fontWeight={700} cursor="pointer" onClick={() => setAuthModalState((prev) => ({
-                    ...prev,
-                    view: "login"
-                }))
-                }>LOG IN</Text>
-                </Flex>
-        </form>
-    ) 
-}
+  const createUserDocument = async (user: User) => {
+    await addDoc(
+      collection(firestore, "users"),
+      JSON.parse(JSON.stringify(user))
+    );
+  };
+
+  useEffect(() => {
+    if (userCred) {
+      createUserDocument(userCred.user);
+    }
+  }, [userCred]);
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Input
+        required
+        name="email"
+        placeholder="email"
+        type="email"
+        mb={2}
+        onChange={onChange}
+      />
+      <Input
+        required
+        name="password"
+        placeholder="password"
+        type="password"
+        mb={2}
+        onChange={onChange}
+      />
+      <Input
+        required
+        name="confirmPassword"
+        placeholder="confirm password"
+        type="confirm password"
+        onChange={onChange}
+      />
+      {(formError || userError) && (
+        <Text textAlign="center" color="red" fontSize="submit">
+          {formError ||
+            FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
+        </Text>
+      )}
+      <Button
+        width="100%"
+        height="36px"
+        type="submit"
+        mt={2}
+        mb={2}
+        isLoading={loading}
+      >
+        Sign Up
+      </Button>
+      <Flex fontSize="9pt" justifyContent="center">
+        <Text mr={1}>Already a user?</Text>
+        <Text
+          color="blue.500"
+          fontWeight={700}
+          cursor="pointer"
+          onClick={() =>
+            setAuthModalState((prev) => ({
+              ...prev,
+              view: "login",
+            }))
+          }
+        >
+          LOG IN
+        </Text>
+      </Flex>
+    </form>
+  );
+};
